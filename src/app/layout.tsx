2da1d67423e5276a062ce7e2cc4c0d9480ef6910
Himanshu Rai <himanshuraimau9@gmail.com>
2024-06-29 01:53:35 +0530

    add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 
@@ -28,7 +29,9 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        {children}</body>
+        {children}
+        <SpeedInsights/>
+        </body>
     </html>
   );
 }
