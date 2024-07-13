913a7fab8386285cc7c28ef4517e29c68292c7e2
AviraLS22 <aviralsharma.sharma2205@gmail.com>
2024-07-14 00:52:18 +0530

    add buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Hero from "@/components/Hero";
 import Navbar from "@/components/navBar";
 import Link from "next/link";
 
@@ -5,7 +6,7 @@ export default function Home() {
   return (
     <div className="h-screen w-full bg-background text-text font-sans">
       <Navbar  />
-      
+      <Hero/>
     </div>
   );
 }
