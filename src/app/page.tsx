0c77328ec4cb3d024d27ef17f6236a16c0b7de00
Himanshu Rai <himanshuraimau9@gmail.com>
2024-07-14 00:32:51 +0530

    remove the acc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import { AccordionDemo } from "@/components/my-comp/myAcc";
 import Navbar from "@/components/navBar";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="h-screen w-full bg-background text-text font-sans">
       <Navbar  />
-      <div className=" text-text space-y-4">
-               <AccordionDemo/>
-      </div>
+      
     </div>
   );
 }
